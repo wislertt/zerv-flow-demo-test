fcebf83b39915ccbc9e6ad77f138473438302cfa
Wisaroot Lertthaweedech <l.wisaroot@gmail.com>
2025-12-10 13:56:46 +0700

    ci: dev

--- .releaserc.js
+++ .releaserc.js
@@ -5,24 +5,35 @@ module.exports = {
             "@semantic-release/commit-analyzer",
             {
                 preset: "angular",
-                parserOpts: {
-                    noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
-                }
-            }
+                releaseRules: [
+                    // Pre-alpha configuration: all commit types trigger patch releases
+                    // TODO: Change feat to "minor" when project reaches stable state
+                    { type: "feat", release: "patch" },
+                    { type: "fix", release: "patch" },
+                    { type: "chore", release: "patch" },
+                    { type: "docs", release: "patch" },
+                    { type: "style", release: "patch" },
+                    { type: "refactor", release: "patch" },
+                    { type: "perf", release: "patch" },
+                    { type: "test", release: "patch" },
+                    { type: "build", release: "patch" },
+                    { type: "ci", release: "patch" },
+                ],
+            },
         ],
         [
             "@semantic-release/release-notes-generator",
             {
                 preset: "angular",
                 parserOpts: {
-                    noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"]
+                    noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES", "BREAKING"],
                 },
                 writerOpts: {
-                    commitsSort: ["subject", "scope"]
-                }
-            }
+                    commitsSort: ["subject", "scope"],
+                },
+            },
         ],
         "@semantic-release/changelog",
-        "@semantic-release/github"
+        "@semantic-release/github",
     ],
 }
